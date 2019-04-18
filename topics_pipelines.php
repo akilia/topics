72221a784d08be80dd0652885d4a8a48fb4ff0c1
Akilia <pm@akilia.net>
2019-04-18 17:09:43 +0200

    Lier des auteurs

--- topics_pipelines.php
+++ topics_pipelines.php
@@ -62,6 +62,57 @@ function topics_affiche_enfants($flux) {
 }
 
 /**
+ * Ajout de contenu sur certaines pages,
+ * notamment des formulaires de liaisons entre objets
+ *
+ * @pipeline affiche_milieu
+ * @param  array $flux Données du pipeline
+ * @return array       Données du pipeline
+ */
+function topics_affiche_milieu($flux) {
+	$texte = '';
+	$e = trouver_objet_exec($flux['args']['exec']);
+
+	// auteurs sur les topic
+	if (!$e['edition'] and in_array($e['type'], array('topic'))) {
+		$texte .= recuperer_fond('prive/objets/editer/liens', array(
+			'table_source' => 'auteurs',
+			'objet' => $e['type'],
+			'id_objet' => $flux['args'][$e['id_table_objet']]
+		));
+	}
+
+
+
+	if ($texte) {
+		if ($p = strpos($flux['data'], '<!--affiche_milieu-->')) {
+			$flux['data'] = substr_replace($flux['data'], $texte, $p, 0);
+		} else {
+			$flux['data'] .= $texte;
+		}
+	}
+
+	return $flux;
+}
+
+/**
+ * Ajout de liste sur la vue d'un auteur
+ *
+ * @pipeline affiche_auteurs_interventions
+ * @param  array $flux Données du pipeline
+ * @return array       Données du pipeline
+ */
+function topics_affiche_auteurs_interventions($flux) {
+	if ($id_auteur = intval($flux['args']['id_auteur'])) {
+		$flux['data'] .= recuperer_fond('prive/objets/liste/topic', array(
+			'id_auteur' => $id_auteur,
+			'titre' => _T('topic:info_topic_auteur')
+		), array('ajax' => true));
+	}
+	return $flux;
+}
+
+/**
  * Afficher le nombre d'éléments dans les parents
  *
  * @pipeline boite_infos
