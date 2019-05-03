ff724b3793b80f975254843186e14ccdb00465e8
Akilia <pm@akilia.net>
2019-05-03 16:58:16 +0200

    Pétouille dans une chaine de langue

--- lang/topic_fr.php
+++ lang/topic_fr.php
@@ -26,7 +26,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_topics_auteur' => 'Les sujets de forum de cet auteur',
 
 	// N
-	'notification_activer' => 'Activer la notifcation par email de la création d’un nouveau sujet',
+	'notification_activer' => 'Activer la notification par email de la création d’un nouveau sujet',
 	'notification_a_qui' => 'À qui ?',
 	'notification_email_from' => 'Adresse email de l’expéditeur',
 	'notification_email_from_explication' => 'Par défaut, c’est l’adresse email du créateur du sujet. Vous pouvez préciser ici une adresse de type ’no-reply’ par exemple.',
